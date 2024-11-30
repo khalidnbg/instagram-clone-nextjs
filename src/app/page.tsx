0bc541ce3cb413ff1da86dc244504b6b26928d03
khalidnbg <khalidnabgaoui4@gmail.com>
2024-12-01 00:28:27 +0100

    testing auth with authJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,42 @@
-export default function Home() {
-  return <div className="bg-ig-red">test</div>;
+import { auth, signIn, signOut } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <div className="">
+      {/* {JSON.stringify(session)} */}
+      test <br />
+      {session && (
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button
+            className="border px-4 py-2 bg-ig-red text-white rounded-lg"
+            type="submit"
+          >
+            Logout
+          </button>
+        </form>
+      )}
+      {!session && (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <button
+            className="border px-4 py-2 bg-ig-red text-white rounded-lg"
+            type="submit"
+          >
+            Login with google
+          </button>
+        </form>
+      )}
+    </div>
+  );
 }
